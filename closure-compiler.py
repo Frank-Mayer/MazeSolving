5947ec2fa00b6ecdb2510221befe7253ba8dde87
Frank Mayer <frank.mayer19072001@outlook.de>
2020-09-05 12:12:03 +0200

    Added tsc compile to closure-compiler

--- closure-compiler.py
+++ closure-compiler.py
@@ -8,29 +8,31 @@ import sys
 import os
 
 directory = r'./'
-for filename in os.listdir(directory):
-    if filename.endswith(".js"):
-        print(filename)
-        file_path = (os.path.join(directory, filename))
-        file_object = open(file_path, "r")
-        code = file_object.read()
-        file_object.close()
-        params = urllib.parse.urlencode([
-            ('js_code', code),
-            ('compilation_level', 'ADVANCED_OPTIMIZATIONS'),
-            ('output_format', 'text'),
-            ('output_info', 'compiled_code'),
-        ])
-        headers = {"Content-type": "application/x-www-form-urlencoded"}
-        conn = http.client.HTTPSConnection('closure-compiler.appspot.com')
-        conn.request('POST', '/compile', params, headers)
-        response = conn.getresponse()
-        data = response.read()
-        code = data.decode("utf-8")
-        print(code)
-        file_object = open(file_path, "w")
-        file_object.write('"use strict";\n'+code)
-        file_object.close()
-        conn.close()
-    else:
-        continue
+if (os.system("tsc -p " + directory + "tsconfig.json --pretty") == 0):
+    print("TypeScript compiled")
+    for filename in os.listdir(directory):
+        if filename.endswith(".js"):
+            print(filename)
+            file_path = (os.path.join(directory, filename))
+            file_object = open(file_path, "r")
+            code = file_object.read()
+            file_object.close()
+            params = urllib.parse.urlencode([
+                ('js_code', code),
+                ('compilation_level', 'ADVANCED_OPTIMIZATIONS'),
+                ('output_format', 'text'),
+                ('output_info', 'compiled_code'),
+            ])
+            headers = {"Content-type": "application/x-www-form-urlencoded"}
+            conn = http.client.HTTPSConnection('closure-compiler.appspot.com')
+            conn.request('POST', '/compile', params, headers)
+            response = conn.getresponse()
+            data = response.read()
+            code = data.decode("utf-8")
+            print("Minified by closure-compiler")
+            file_object = open(file_path, "w")
+            file_object.write('"use strict";\n'+code)
+            file_object.close()
+            conn.close()
+        else:
+            continue
